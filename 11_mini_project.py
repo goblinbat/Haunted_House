904348ff25fa37d5ca09fe044a9d8063c2bc23a8
Molly Hopper <mollyhopper95@gmail.com>
2019-02-07 14:46:39 -0500

    continue

--- 11_mini_project.py
+++ 11_mini_project.py
@@ -10,27 +10,48 @@ def menu():
 def choice0(option):
     if option == '1':
         print('Your car breaks down in the middle of some dark spooky woods... also its raining. \n'
-        'In the distance you see a house. Ignoring the fact this is the setup to a bad horror movie \n'
+        'In the distance you see a house. \n'
         'would you like to approach the house?')
         print('1) Go to the house? \n'
               '2) Leave?')
-        input('>')
 
     elif option == '2':
         exit(1)
     else:
         print("invalid input; try again.")
-        menu()
+        choice0(options)
 
-#def choice1()
+def choice1(option):
+    if option == '1':
+        print('Ignoring the fact this is the setup to a bad horror movie, you approach the house. \n')
+        print('You are soaked to the bone by the time you reach the front door.\n'
+              "Trying to open it and get out of the rain, you find it's locked.\n"
+              'Fortunately, you spot a cellar door off to the side. Try and open it? \n')
+        print('1) try and open the cellar door \n'
+              '2) knock on the front door \n'
+              '3) try and break down the front door \n')
+    if option == '2':
+        print("too bad, there's nowhere else to go.")
+        print('You are soaked to the bone by the time you reach the front door.\n'
+              "Trying to open it and get out of the rain, you find it's locked.\n"
+              'Fortunately, you spot a cellar door off to the side. Try and open it? \n')
+        print('1) try and open the cellar door \n'
+              '2) knock on the front door \n'
+              '3) try and break down the front door \n')
+    else:
+        print('invalid input; try again.')
+        choice1(options)
+
 
 menu()
 
 options = input('input your choice: \n >')
 
 choice0(options)
+options = input('input your choice: \n >')
 
-#choice1()
+choice1(options)
+options = input('input your choice: \n >')
 
 
 
