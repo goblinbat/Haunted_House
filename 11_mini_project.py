7b3ead7f2e89d30d134e11cffa0b5550c5fa960b
Molly Hopper <mollyhopper95@gmail.com>
2019-02-07 14:10:06 -0500

    start

--- 11_mini_project.py
+++ 11_mini_project.py
@@ -1,2 +1,31 @@
 # Haunted House Text Game
 
+import os
+
+def menu():
+    print("The Haunted House \n" +
+          "1) Play \n" +
+          "2) Exit\n")
+    x = input('input your choice: \n' +
+          '>')
+
+menu()
+def choice0(option):
+    option = x
+    if option == '1':
+        print('print')
+    elif option == '2':
+        exit(1)
+    else:
+        print("invalid input; try again.")
+        menu()
+
+
+
+#def choice1():
+#    print("intro description")
+
+# while True:
+#     os.system('cls||clear')
+
+
