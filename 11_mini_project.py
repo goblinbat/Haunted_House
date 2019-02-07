9aa727048c4774e9c29dc7eadbea64fe23fe3f5d
Xzavier Dunn <xmandunn@gmail.com>
2019-02-07 14:30:22 -0500

    Updated

--- 11_mini_project.py
+++ 11_mini_project.py
@@ -6,19 +6,32 @@ def menu():
     print("The Haunted House \n" +
           "1) Play \n" +
           "2) Exit\n")
-    x = input('input your choice: \n' +
-          '>')
 
-menu()
 def choice0(option):
-    option = x
     if option == '1':
-        print('print')
+        print('Your car breaks down in the middle of some dark spooky woods... also its raining. \n'
+        'In the distance you see a house. Ignoring the fact this is the setup to a bad horror movie \n'
+        'would you like to approach the house?')
+        print('1) Go to the house? \n'
+              '2) Leave?')
+        input('>')
+
     elif option == '2':
         exit(1)
     else:
         print("invalid input; try again.")
         menu()
+
+#def choice1()
+
+menu()
+
+options = input('input your choice: \n >')
+
+choice0(options)
+
+#choice1()
+
 
 
 
